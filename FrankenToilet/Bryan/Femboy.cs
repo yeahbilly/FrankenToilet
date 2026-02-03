fe142129d93bbdb6776e7693b186a3527cbf07a8
Bryan_-000- <fredayddd321ewq@gmail.com>
2026-02-03 00:03:55 +0000

    trans lust

--- FrankenToilet/Bryan/Femboy.cs
+++ FrankenToilet/Bryan/Femboy.cs
@@ -33,6 +33,38 @@ public class Femboy
             if (SceneHelper.CurrentScene == "Level 7-1")
                 FindObject<Image>("Canvas/HurtScreen/White").sprite = BundleLoader.Flash;
 
+            if (SceneHelper.CurrentScene == "Level 2-1")
+            {
+                Transform Room1Lighting = FindObject<Transform>("1 - New Opener/1 Nonstuff/Lighting");
+                Room1Lighting.GetChild(0).GetComponent<Light>().color = new(0f, 0.8f, 1f, 1f);
+                Room1Lighting.GetChild(1).GetComponent<Light>().color = new(0.86f, 0f, 0.5f, 1f);
+
+                // room1 section 2
+                Room1Lighting.GetChild(2).GetComponent<Light>().color = new(0.96f, 0.63f, 1f, 1f);
+                Room1Lighting.GetChild(3).GetComponent<Light>().color = new(0.314159265f, 0.58f, 1f, 1f);
+                Room1Lighting.GetChild(5).GetComponent<Light>().color = new(1f, 0.86f, 1f, 1f);
+
+                Transform Room2Decor = FindObject<Transform>("2 - Tower 1/2 Nonstuff/Decorations");
+                Room2Decor.GetChild(0).Find("Point Light").GetComponent<Light>().color = new(0f, 0.8f, 1f, 1f);
+                Room2Decor.GetChild(1).Find("Point Light").GetComponent<Light>().color = new(1f, 0.86f, 1f, 1f);
+                Room2Decor.GetChild(2).Find("Point Light").GetComponent<Light>().color = new(0.86f, 0f, 0.5f, 1f);
+                Room2Decor.GetChild(3).Find("Point Light").GetComponent<Light>().color = new(0.314159265f, 0.58f, 1f, 1f);
+                Room2Decor.GetChild(4).Find("Point Light").GetComponent<Light>().color = new(0.86f, 0f, 0.5f, 1f);
+                Room2Decor.GetChild(5).Find("Point Light").GetComponent<Light>().color = new(0f, 0.81f, 1f, 1f);
+                Room2Decor.GetChild(6).Find("Point Light").GetComponent<Light>().color = new(0.96f, 0.63f, 1f, 1f);
+                Room2Decor.GetChild(7).Find("Point Light").GetComponent<Light>().color = new(0f, 1f, 1f, 1f);
+                Room2Decor.GetChild(8).Find("Point Light").GetComponent<Light>().color = new(1f, 0.86f, 1f, 1f);
+                Room2Decor.GetChild(9).Find("Point Light").GetComponent<Light>().color = new(0.314159265f, 0.58f, 1f, 1f);
+                Room2Decor.GetChild(10).Find("Point Light").GetComponent<Light>().color = new(0.86f, 0f, 0.5f, 1f);
+                Room2Decor.GetChild(11).Find("Point Light").GetComponent<Light>().color = new(0.96f, 0.63f, 1f, 1f);
+
+                Transform Room5Lighting = FindObject<Transform>("5 - Tower 2/5 Nonstuff/Lights");
+                for (int i = 0; i < 20; i++)
+                {
+                    Room5Lighting.GetChild(i).Find("Point Light").GetComponent<Light>().color = new(0.92f, 0.4f, 0.74f, 1f);
+                }
+            }
+
             FindObject<TextMeshProUGUI>("Canvas/Level Stats Controller/Level Stats (1)/Style Title")?.text = "AURA";
         };
     }
---
+++ FrankenToilet/Bryan/Patches/OutdoorLightMasterPatch.cs
@@ -0,0 +1,26 @@
+﻿namespace FrankenToilet.Bryan.Patches;
+
+using FrankenToilet.Core;
+using HarmonyLib;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+/// <summary> Assigns outdoors light colors for 2-1. </summary>
+[PatchOnEntry]
+[HarmonyPatch(typeof(OutdoorLightMaster))]
+public static class OutdoorLightMasterPatch
+{
+    /// <summary> assign colors cuz they look cool :3 </summary>
+    [HarmonyPostfix]
+    [HarmonyPatch("Start")]
+    public static void raaaaaaawruwu(OutdoorLightMaster __instance)
+    {
+        if (SceneHelper.CurrentScene == "Level 2-1")
+        {
+            __instance.outdoorLights[0].color = new Color(0.86f, 0f, 0.5f, 1f);
+            __instance.outdoorLights[1].color = new Color(0f, 0.81f, 1f, 1f);
+            __instance.outdoorLights[2].color = new Color(0f, 1f, 1f, 1f);
+        }
+    }
+}
